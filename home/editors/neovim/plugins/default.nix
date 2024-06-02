08486dc9a9855498c28b79cd35100f5b0854ac14
NatProgramer <rodrigezsantiago40@gmail.com>
2024-06-02 21:11:22 +0000

    delete neocord file to be inside of default.nix

--- home/editors/neovim/plugins/default.nix
+++ home/editors/neovim/plugins/default.nix
@@ -1,6 +1,18 @@
 {
+  # Plugin manager
   lazy = import ./lazy.nix;
+
+	# Editor utils
+	treesiter = import ./treesitter.nix;
+	neoformat = import ./neoformat.nix;
+
+
+	# User interface (UI)
+	noice = import ./noice.nix;
   lualine = import ./lualine.nix;
-  neocord = import ./neocord.nix;
   neo-tree = import ./neotree.nix;
+	bufferline = import ./bufferline.nix;
+
+	# Misc
+  neocord = { enable = true; };
 }
