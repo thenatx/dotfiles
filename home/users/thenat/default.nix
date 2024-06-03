c3f11e6e94ee8a7a232757ba7abad5ddf1e3d74e
NatProgramer <rodrigezsantiago40@gmail.com>
2024-06-03 21:27:10 +0000

    quit programs not use frequently

--- home/users/thenat/default.nix
+++ home/users/thenat/default.nix
@@ -1,11 +1,11 @@
 {self, ...}: let
   home = "${self}/home";
+  editors = "${home}/editors";
   terminal = {
     emulators = "${home}/terminal/emulators";
     programs = "${home}/terminal/programs";
     shell = "${home}/terminal/shell";
   };
-  editors = "${home}/editors";
 in {
   imports = [
     # General software
@@ -14,17 +14,17 @@ in {
     # Terminal programs
     "${terminal.emulators}/kitty"
     "${terminal.emulators}/wezterm"
-    "${terminal.emulators}/alacritty"
+
+    # "${terminal.emulators}/alacritty"
 
     "${terminal.programs}"
     "${terminal.shell}"
 
     # Code editors
     "${editors}/zed"
-    "${editors}/helix"
-    "${editors}/vscode"
-
-    # Incompleted, you can comment this
     "${editors}/neovim"
+
+    # "${editors}/vscode"
+    # "${editors}/helix"
   ];
 }
