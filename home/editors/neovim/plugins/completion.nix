8a8594e7f2d3db7ac92ccc4fd563a61a96fe1998
NatProgramer <rodrigezsantiago40@gmail.com>
2024-06-06 22:41:53 +0000

    fix error cmp.cmp no exists

--- home/editors/neovim/plugins/completion.nix
+++ home/editors/neovim/plugins/completion.nix
@@ -1,49 +1,47 @@
 {
-	cmp = {
-		enable = true;
+  enable = true;
 
-		settings = {
-			mapping = {
-				__raw = ''
-          cmp.mapping.preset.insert({
-            ['<C-j>'] = cmp.mapping.select_next_item(),
-            ['<C-k>'] = cmp.mapping.select_prev_item(),
-            ['<C-e>'] = cmp.mapping.abort(),
- 
-            ['<C-f>'] = cmp.mapping.scroll_docs(-4),
+  settings = {
+    mapping = {
+      __raw = ''
+        cmp.mapping.preset.insert({
+          ['<C-j>'] = cmp.mapping.select_next_item(),
+          ['<C-k>'] = cmp.mapping.select_prev_item(),
+          ['<C-e>'] = cmp.mapping.abort(),
 
-            ['<C-g>'] = cmp.mapping.scroll_docs(4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(-4),
 
-            ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-g>'] = cmp.mapping.scroll_docs(4),
 
-            ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ['<C-Space>'] = cmp.mapping.complete(),
 
-            ['<S-CR>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-          })
-        '';
-			};
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
 
-			snippet = { 
-				expand = "function(args) require('luasnip').lsp_expand(args.body) end";
-			};
+          ['<S-CR>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+        })
+      '';
+    };
 
-      sources = {
-        __raw = ''
-          cmp.config.sources({
-            {name = 'nvim_lsp'},
-            {name = 'path'},
-            {name = 'luasnip'},
-            {name = 'cmdline'},
-            }, {
-          {name = 'buffer'},
-          })
-        '';
-      };
+    snippet = {
+      expand = "function(args) require('luasnip').lsp_expand(args.body) end";
+    };
 
-		  window = {
-				completion.border = "rounded";
-				documentation.border = "rounded";
-			};
-		};
-	};
+    sources = {
+      __raw = ''
+        cmp.config.sources({
+          {name = 'nvim_lsp'},
+          {name = 'path'},
+          {name = 'luasnip'},
+          {name = 'cmdline'},
+          }, {
+        {name = 'buffer'},
+        })
+      '';
+    };
+
+    window = {
+      completion.border = "rounded";
+      documentation.border = "rounded";
+    };
+  };
 }
