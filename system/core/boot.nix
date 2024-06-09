acc914e16e63ff7b61a03c5746085d6fc3f5cab0
NatProgramer <rodrigezsantiago40@gmail.com>
2024-06-09 00:16:16 +0000

    change theme names

--- system/core/boot.nix
+++ system/core/boot.nix
@@ -6,9 +6,9 @@
     sha256 = "sha256-yOvZ4F5ERPfnSlI/Scf9UwzvoRwGMqZlrHkBIB3Dm/w=";
   };
 
-  mac-style-load = pkgs.callPackage mac-style-src {};
+  mac-style = pkgs.callPackage mac-style-src {};
 
-  catppuccin-grub-src = pkgs.fetchFromGitHub {
+  catppuccin-grub = pkgs.fetchFromGitHub {
     owner = "catppuccin";
     repo = "grub";
     rev = "88f6124757331fd3a37c8a69473021389b7663ad";
@@ -24,14 +24,14 @@ in {
         device = "nodev";
         efiSupport = true;
         backgroundColor = "#8277E5";
-        theme = "${catppuccin-grub-src}/src/catppuccin-mocha-grub-theme";
+        theme = "${catppuccin-grub}/src/catppuccin-mocha-grub-theme";
       };
     };
 
     plymouth = {
       enable = true;
       theme = "mac-style";
-      themePackages = [mac-style-load];
+      themePackages = [mac-style];
       font = "${pkgs.comic-mono}/share/fonts/ComicMono.ttf";
     };
   };
