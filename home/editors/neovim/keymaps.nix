3de964e34b65257d5356322388b6c6fc71bf9d17
NatProgramer <rodrigezsantiago40@gmail.com>
2024-06-10 21:09:42 +0000

    define keymaps

--- home/editors/neovim/keymaps.nix
+++ home/editors/neovim/keymaps.nix
@@ -1,12 +1,12 @@
 [
-	{
-		action = ":SSSelected";
-		key = "<Leader>s"; 
-		mode = "v";
-	}
-	{
-		action =":SSFocused";
-		key = "<Leader>s";
-		mode = "n";
-	}
+  {
+    action = ":SSSelected";
+    key = "<Leader>s";
+    mode = "v";
+  }
+  {
+    action = ":SSFocused";
+    key = "<Leader>s";
+    mode = "n";
+  }
 ]
