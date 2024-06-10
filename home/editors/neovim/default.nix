dab0aadf4dcd950911524dbbea118aabaa4026e0
NatProgramer <rodrigezsantiago40@gmail.com>
2024-06-10 01:27:49 +0000

    add codeshot to plugins

--- home/editors/neovim/default.nix
+++ home/editors/neovim/default.nix
@@ -1,32 +1,35 @@
-{inputs, pkgs, ...}: let
-	toLua = raw: "lua << EOF\n${raw}\nEOF\n";
+{
+  inputs,
+  pkgs,
+  ...
+}: let
+  toLua = raw: "lua << EOF\n${raw}\nEOF\n";
 
-	plugins = { 
-		codeshot-src = pkgs.fetchFromGitHub {
-		  owner = "sergioribera";
-		  repo = "codeshot.nvim";
-		  rev = "7e418c2b6e8b7aaa75d41c5c91d96a837251a18d";
-		  hash = "sha256-xMyn64uLZ0Gpnpt5ZGSPm/6F8bHdVezqlmvCAjwXmeA=";
-		};
+  plugins = {
+    codeshot-src = pkgs.fetchFromGitHub {
+      owner = "sergioribera";
+      repo = "codeshot.nvim";
+      rev = "7e418c2b6e8b7aaa75d41c5c91d96a837251a18d";
+      hash = "sha256-xMyn64uLZ0Gpnpt5ZGSPm/6F8bHdVezqlmvCAjwXmeA=";
+    };
 
-		cord-nvim-src = pkgs.fetchFromGitHub {
-  		owner = "vyfor";
-		  repo = "cord.nvim";
-		  rev = "cf0af0fc9b8e218afd8c4928ae2e711ce455181b";
-		  hash = "sha256-Ev44plJGhZ8wY3hcWg6BOV2T3g38JhctJ8KozDnAkUM=";
-		};
-	};
+    cord-nvim-src = pkgs.fetchFromGitHub {
+      owner = "vyfor";
+      repo = "cord.nvim";
+      rev = "cf0af0fc9b8e218afd8c4928ae2e711ce455181b";
+      hash = "sha256-Ev44plJGhZ8wY3hcWg6BOV2T3g38JhctJ8KozDnAkUM=";
+    };
+  };
 
-	codeshot = pkgs.vimUtils.buildVimPlugin {
-		name = "codeshot";
-		src = plugins.codeshot-src;
-	};
-	
-	cord-nvim = pkgs.vimUtils.buildVimPlugin {
-		name = "cord.nvim";
-		src = plugins.cord-nvim-src;
-	};
+  codeshot = pkgs.vimUtils.buildVimPlugin {
+    name = "codeshot";
+    src = plugins.codeshot-src;
+  };
 
+  cord-nvim = pkgs.vimUtils.buildVimPlugin {
+    name = "cord.nvim";
+    src = plugins.cord-nvim-src;
+  };
 in {
   imports = [
     inputs.nixvim.homeManagerModules.nixvim
@@ -43,31 +46,28 @@ in {
 
     opts = import ./opts.nix;
     plugins = import ./plugins;
+		keymaps = import ./keymaps.nix;
     colorschemes = import ./colorscheme.nix;
 
-		extraPlugins = [
-			{
-				plugin = codeshot;
-				config = toLua ''
-          require("codeshot").setup({
-						author = "@Thenat",
-            background = "#F1D3DE",
-            author_color = '#000',
-            output = "raw",
-
-            silent = true,
-						copy = "%c | wl-copy",
-            window_controls = false,
-            show_line_numbers = false,
-            use_current_theme = false,
-            theme = 'Catppuccin Mocha',
-          })
-				'';
-			}
-#			{
-#				plugin = cord-nvim;
-#				config = toLua "require(\"cord\").setup()";
-#			}
-		];
-	};
+    extraPlugins = [
+      {
+        plugin = codeshot;
+        config = toLua ''
+                 require("codeshot").setup({
+						       copy = "%c | wl-copy";
+      						 fonts = "";
+									 shadow_image = true;
+      						 use_current_theme = true;
+						       background = "#AAAAFF";
+						       author = "@SergioRibera";
+						       author_color = "#000";
+                 })
+        '';
+      }
+      #			{
+      #				plugin = cord-nvim;
+      #				config = toLua "require(\"cord\").setup()";
+      #			}
+    ];
+  };
 }
---
+++ home/editors/neovim/keymaps.nix
@@ -0,0 +1,12 @@
+[
+	{
+		action = ":SSSelected";
+		key = "<Leader>s"; 
+		mode = "v";
+	}
+	{
+		action =":SSFocused";
+		key = "<Leader>s";
+		mode = "n";
+	}
+]
