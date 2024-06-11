70e53e2e5d2a34b47f2b4fa7400f9cee2fb750b4
NatProgramer <rodrigezsantiago40@gmail.com>
2024-06-11 22:20:40 +0000

    put custom plugins on the plugins folder

--- home/editors/neovim/default.nix
+++ home/editors/neovim/default.nix
@@ -1,28 +1,8 @@
 {
   inputs,
-  pkgs,
   ...
 }: let
   toLua = raw: "lua << EOF\n${raw}\nEOF\n";
-
-  plugins = {
-    codeshot-src = pkgs.fetchFromGitHub {
-      owner = "sergioribera";
-      repo = "codeshot.nvim";
-      rev = "7e418c2b6e8b7aaa75d41c5c91d96a837251a18d";
-      hash = "sha256-xMyn64uLZ0Gpnpt5ZGSPm/6F8bHdVezqlmvCAjwXmeA=";
-    };
-  };
-
-  codeshot = pkgs.vimUtils.buildVimPlugin {
-    name = "codeshot";
-    src = plugins.codeshot-src;
-  };
-
-	cord = pkgs.vimUtils.buildVimPlugin { 
-		name = "cord.nvim";
-		src = pkgs.callPackage ./plugins/cord.nix;
-	};
 in {
   imports = [
     inputs.nixvim.homeManagerModules.nixvim
@@ -44,21 +24,21 @@ in {
 
     extraPlugins = [
       {
-        plugin = codeshot;
+        plugin = ./plugins/codeshot.nix;
         config = toLua ''
-               	 require("codeshot").setup({
-           	 copy = "%c | wl-copy";
-             fonts = "";
-          shadow_image = true;
-             use_current_theme = true;
-             background = "#AAAAFF";
-             author = "@SergioRibera";
-             author_color = "#000";
-               })
+         	 	require("codeshot").setup({
+           	 	copy = "%c | wl-copy";
+             	fonts = "";
+          		shadow_image = true;
+             	use_current_theme = true;
+	            background = "#AAAAFF";
+             	author = "@Thenat";
+             	author_color = "#000";
+          	})
         '';
       }
       {
-       	plugin = cord;
+       	plugin = ./plugins/cord.nix;
        	config = toLua "require(\"cord\").setup()";
       }
     ];
--- home/editors/neovim/plugins/cord.nix
+++ home/editors/neovim/plugins/cord.nix
@@ -1,17 +1,17 @@
 { 
-	stdenv,
 	pkgs
 }: let
-	cord-nvim-src = pkgs.fetchFromGitHub {
+	cord-src = pkgs.fetchFromGitHub {
     owner = "vyfor";
     repo = "cord.nvim";
     rev = "cf0af0fc9b8e218afd8c4928ae2e711ce455181b";
     hash = "sha256-Ev44plJGhZ8wY3hcWg6BOV2T3g38JhctJ8KozDnAkUM=";
   };
-in stdenv.mkDerivation {
+
+	cord-pkg = { stdenv, ... }: stdenv.mkDerivation {
     pname = "cord.nvim";
 		version = "v1.0";
-    src = cord-nvim-src;
+    src = cord-src;
 
 		buildInputs = with pkgs; [ rustc gcc ];
 
@@ -21,6 +21,14 @@ in stdenv.mkDerivation {
 
 		installPhase = ''
 			mkdir $out
-			cp cord $out/cord
+			cp -r * $out
+			mv $out/cord $out/cord.so
 		'';
+	};
+in 
+
+pkgs.vimUtils.buildVimPlugin {
+		name = "cord.nvim";
+		src = pkgs.callPackage cord-pkg {};
 }
+
