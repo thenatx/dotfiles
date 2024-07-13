7cc5da4322a58a76616f3137f6c6c60bedf2d2f7
NatProgramer <rodrigezsantiago40@gmail.com>
2024-07-13 00:50:38 +0000

    add korean, chinasse and japan fonts

--- system/programs/fonts.nix
+++ system/programs/fonts.nix
@@ -1,12 +1,19 @@
 {pkgs, ...}: {
   fonts = {
     fontconfig = {
-      defaultFonts.emoji = ["Twitter Color Emoji" "Noto Color Emoji"];
+      defaultFonts = {
+       	serif = [ "Noto Serif" "Noto Color Emoji"];
+        sansSerif = ["Lexend" "Noto Color Emoji"];
+        monospace = ["GeistMono Nerd Font" "Noto Color Emoji"];
+        emoji = ["Noto Color Emoji"];
+			};
     };
 
     packages = with pkgs; [
-      twitter-color-emoji
-      noto-fonts-color-emoji
+			lexend
+	    noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
 
       comic-mono
       (nerdfonts.override {
