1d0bf3e08056eac76e362646eec2fb1335d22a11
NatProgramer <rodrigezsantiago40@gmail.com>
2024-10-26 16:38:33 -0500

    Change deprecated configs for eza and nvim

--- home/editors/neovim/plugins/lsp.nix
+++ home/editors/neovim/plugins/lsp.nix
@@ -6,16 +6,22 @@
 
     cssls.enable = true;
 
-    tsserver.enable = true;
+    ts_ls.enable = true;
 
-    rust-analyzer.enable = true;
+    rust_analyzer = {
+      enable = true;
+      # This two options will make cargo and rustc not installed automatically
+      # you should use devshells instead but can set it to true to install both and no install via 'home.packages'
+      installCargo = false;
+      installRustc = false;
+    };
 
     volar.enable = true;
 
     nil_ls.enable = true;
 
-    # bashls = {
-    #   enable = true;
-    # };
+    bashls = {
+      enable = true;
+    };
   };
 }
